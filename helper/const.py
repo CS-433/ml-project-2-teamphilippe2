e54d75507cdc55c57f01d2c41880a7fd4aa0c625
AlessioVerardo <44498464+AlessioVerardo@users.noreply.github.com>
2021-11-29 15:28:31 +0100

    Use const.py to give a structure to the project

---
+++ helper/const.py
@@ -0,0 +1,24 @@
+# Folder path
+root_dir = "data/"
+training_dir = root_dir +"training/"
+image_dir = training_dir + "images/"
+gt_dir = training_dir + "groundtruth/"
+output_folder = "output/"
+weights_folder = output_folder+ "weights/"
+submission_folder = output_folder + "submissions/"
+
+
+# extension weight models
+ext_weight_model = ".pth"
+
+# Path to the best model weights
+best_model_weight_path = weights_folder + "unet/test"+ext_weight_model
+
+# Testing images related constants
+test_dir = root_dir + "test_set_images/"
+test_image_prefix = "test_"
+test_image_suffix = ".jpg"
+
+# Size of images used as input to the neural network
+train_img_height = 400
+train_img_width = 400
