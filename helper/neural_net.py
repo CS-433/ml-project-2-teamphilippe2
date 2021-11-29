2b97def9d89d8a3718cd38a2c23bae0e446d7029
AlessioVerardo <44498464+AlessioVerardo@users.noreply.github.com>
2021-11-29 15:27:04 +0100

    run_experiment created

--- helper/neural_net.py
+++ helper/neural_net.py
@@ -1,3 +1,8 @@
+import torch
+from datetime import datetime
+from helper.metrics import *
+from helper.const import *
+
 def train(model, criterion, dataset_train, dataset_test, optimizer, num_epochs, print_iteration=True):
     """
     Fully train a neural network
@@ -57,3 +62,62 @@ def train(model, criterion, dataset_train, dataset_test, optimizer, num_epochs, 
             print("Epoch {} | Test accuracy: {:.5f}".format(epoch, sum(accuracies_test).item()/len(accuracies_test)))
     print("End of training")
     return model
+
+
+def loss_function_from_string(loss_fct_str):
+    if loss_fct_str == "CrossEntropyLoss":
+        return torch.nn.CrossEntropyLoss()
+    else:
+        return None
+    
+def model_from_string(model_str):
+    if model_str == "unet":
+        return torch.nn.CrossEntropyLoss()
+    else:
+        return None
+
+def optimizer_from_string(optimizer_str, params, lr,momentum):
+    if optimizer_str == "Adam":
+        return torch.optim.Adam(params, lr=learning_rate, momentum=momentum)
+    elif optimizer_str=="SGD":
+        return torch.optim.SGD(params, lr=learning_rate, momentum=momentum)
+    else:
+        return None
+
+def run_experiment(model_str, loss_fct_str, optimizer_str, image_dir, gt_dir, num_epochs=1, learning_rate=1e-3, momentum=0.0, batch_size = 1000, save_weights=True,ratio_test=0.2, seed=1):
+    num_epochs = 10
+    learning_rate = 1e-3
+    batch_size = 1000
+    
+    ds = AugmentedRoadImages(image_dir, gt_dir, ratio_test)
+    
+    dataset_train = torch.utils.data.DataLoader(ds,
+      batch_size=batch_size,
+      shuffle=True
+    )
+
+    # If a GPU is available, use it
+    if not torch.cuda.is_available():
+        print("Things will go much quicker with a GPU")
+        device = torch.device("cpu")
+    else:
+        device = torch.device("cuda")
+
+    # Train the logistic regression model with the Adam optimizer
+    criterion = loss_function_from_string(loss_fct_str)
+    model = LogisticRegressionModel().to(device)
+    optimizer = optimizer_from_string(optimizer_str, model.parameters(), learning_rate,momentum)
+    
+    train(model, criterion, dataset_train, dataset_test, optimizer, num_epochs)
+    
+    if save_weights:
+        now = datetime.now()
+        torch.save(model.state_dict(), weights_folder+model_str+"/"+now.strftime("%Y-%m-%d_%H-%M-%S")+ext_weight_model)
+    
+    # Compute scores on the local test set 
+    img_test, gt_test = ds.get_test_set()
+    predict_test_set_nn(img_test, model)
+    
+    # Display scores
+    _, _, _, _ = compute_scores(preds, gt_test)
+    
