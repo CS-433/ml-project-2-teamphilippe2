a8c7ce822008e98c63c4379e1949a3950d374273
AlessioVerardo <44498464+AlessioVerardo@users.noreply.github.com>
2021-11-29 21:54:05 +0100

    End of comments

--- helper/submission.py
+++ helper/submission.py
@@ -3,6 +3,16 @@ foreground_threshold = 0.25 # percentage of pixels > 1 required to assign a fore
 
 # assign a label to a patch
 def patch_to_label(patch):
+    """
+    Return the value assigned to the patch
+    Parameters:
+    -----------
+        - Patch:
+            array containing 0 or 1
+    Returns: 
+    -----------
+        The assigned value
+    """
     df = np.mean(patch)
     if df > foreground_threshold:
         return 1
@@ -10,7 +20,20 @@ def patch_to_label(patch):
         return 0
 
 def pred_to_submission_strings(img_id, img, patch_size):
-    """Reads a single image and outputs the strings that should go into the submission file"""
+    """
+    Reads a single image and outputs the strings that should go into the submission file
+    Parameters:
+    -----------
+        - img_id: 
+            The id of the image
+        - img: 
+            The image
+        - patch_size: 
+            size of the patch
+    Returns:
+    -----------
+        the strings that should go into the submission file
+    """
     # Convert tensor to numpy array
     img_np = img.numpy()
     
@@ -25,7 +48,19 @@ def pred_to_submission_strings(img_id, img, patch_size):
 
 
 def predictions_to_submission(submission_filename, ids, predictions, patch_size=16):
-    """Converts images into a submission file"""
+    """
+    Converts images into a submission file
+    Parameters:
+    -----------
+        - submission_filename:
+            the file where to write the submission
+        - ids:
+            The ids of all test images
+        - predictions:
+            The predictions on the test images
+        - patch_size:
+            Size of the image patch
+    """
     with open(submission_filename, 'w') as f:
         f.write('id,prediction\n')
         for img_id, pred in zip(ids, predictions):
