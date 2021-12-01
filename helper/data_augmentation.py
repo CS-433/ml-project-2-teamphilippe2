9d48c9284e48fb723b510a16a6d8e28617fc580e
AlessioVerardo <44498464+AlessioVerardo@users.noreply.github.com>
2021-12-01 18:10:39 +0100

    Transsform test dataset as dataset

--- helper/data_augmentation.py
+++ helper/data_augmentation.py
@@ -1,19 +1,20 @@
 import torch
-
-from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
-
-from torchvision import transforms
-from torchvision import datasets
-
 from torchvision.transforms import *
 from torchvision.transforms.functional import *
 
-import matplotlib.pyplot as plt
-import random
-
 from helper.loading import *
-from helper.visualisations import *
+
+
+class RoadTestImages(Dataset):
+    def __init__(self, augmented_ds):
+        self.test_data, self.test_ground_truth = augmented_ds.get_test_set()
+
+    def __len__(self):
+        return len(self.test_data)
+
+    def __getitem__(self, index):
+        return self.test_data[index], self.test_ground_truth[index]
 
 
 class AugmentedRoadImages(Dataset):
@@ -41,7 +42,10 @@ class AugmentedRoadImages(Dataset):
         # Split the train images into train and test set
         imgs_tr, gt_imgs_tr, imgs_te, gt_imgs_te = split_data(imgs, gt_imgs, ratio_test, seed=seed)
 
-        self.test_set = imgs_te, gt_imgs_te
+        # Set the test set and transform the images to tensor and permute ground_truth data.
+        self.test_set = self.to_tensor_and_permute(imgs_te), [self.cap_ground_truth(torch.from_numpy(gt_te)) for gt_te in gt_imgs_te]
+        # Transform the training images to tensor and permute the axes to obtain (3, H, W)
+        imgs_tr = self.to_tensor_and_permute(imgs_tr)
 
         self.all_imgs = []
         self.gt_imgs = []
@@ -56,19 +60,46 @@ class AugmentedRoadImages(Dataset):
         self.n_samples = len(self.all_imgs)
 
     def __len__(self):
-        return self.n_samples
+        return 1  # self.n_samples
 
     def __getitem__(self, index):
         return self.all_imgs[index], self.gt_imgs[index]
 
+    def cap_ground_truth(self, gt_img):
+        gt_img[gt_img > 0] = 1
+        gt_img[gt_img != 1] = 0
+        return gt_img
+
     def get_test_set(self):
-        return self.test_set
+        """
+        Return the test set corresponding to our dataset
+        Returns:
+            The test set
+        """
+        return self.test_set[0], self.test_set[1]
+
+    def to_tensor_and_permute(self, imgs):
+        imgs_new = []
+        for img in imgs:
+            # Transform to tensor and transform (H,W,3) into (3,H,W)
+            img = torch.from_numpy(img)
+            img = torch.permute(img, (2, 0, 1))
+            imgs_new.append(img)
+        return imgs_new
 
     def transform(self, img, gt):
-        # Transform to tensor and transform (H,W,3) into (3,H,W)
-        img = torch.from_numpy(img)
-        img = torch.permute(img, (2, 0, 1))
-        # Create a 3D tesnsor from the groundtruth image
+        """
+        Return multiple transformations of the same image and teh ground truth
+        Parameters:
+        -----------
+            - img: the image we want to transform
+            - gt : the corresponding ground truth image
+        Return:
+            - List of transformations (tensors) of the image
+            - List of transformations (tensors) of the ground truth image
+        """
+
+        # Create a 3D tensor from the ground truth image
         gt = torch.unsqueeze(torch.from_numpy(gt), dim=0).expand(img.shape)
 
         imgs = [img]
@@ -90,7 +121,11 @@ class AugmentedRoadImages(Dataset):
             imgs.append(rotate(img, angle))
             gt_imgs.append(rotate(gt, angle))
 
-        # Only select the first dimension to transform back the groundtruth to a 2D image
-        gt_imgs = [gt_img[0, :, :] for gt_img in gt_imgs]
+        # Only select the first dimension to transform back the ground truth to a 2D image
+        gt_imgs2 = []
+        for gt_img in gt_imgs:
+            gt = gt_img[0, :, :]
+            gt = self.cap_ground_truth(gt)
+            gt_imgs2.append(gt[None, :, :])
 
-        return imgs, gt_imgs
+        return imgs, gt_imgs2
